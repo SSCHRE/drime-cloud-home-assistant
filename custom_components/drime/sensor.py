6cb6ab99c106d954af1e31f68dced2dae7ccfc32
SSCHRE <88311638+SSCHRE@users.noreply.github.com>
2026-02-19 06:41:31 +0100

    Merge usage into single coordinator

--- custom_components/drime/sensor.py
+++ custom_components/drime/sensor.py
@@ -9,11 +9,15 @@ from .coordinator import DrimeDataCoordinator
 from .const import CONF_API_KEY
 
 _LOGGER = logging.getLogger(__name__)
+USAGE_API_URL = "https://app.drime.cloud/api/v1/user/space-usage"
 NOTES_API_URL = "https://app.drime.cloud/api/v1/notes"
 FILES_API_URL = "https://app.drime.cloud/api/v1/track/all?perPage=50"
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     api_key = config_entry.data[CONF_API_KEY]
+
+    usage_coordinator = DrimeDataCoordinator(hass, api_key, USAGE_API_URL)
+    await usage_coordinator.async_config_entry_first_refresh()
 
     notes_coordinator = DrimeDataCoordinator(hass, api_key, NOTES_API_URL)
     await notes_coordinator.async_config_entry_first_refresh()
@@ -22,10 +26,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     await tracked_files_coordinator.async_config_entry_first_refresh()
 
     sensors = [
-        DrimeUsedSensor(hass, api_key),
-        DrimeAvailableSensor(hass, api_key),
-        DrimeTotalSensor(hass, api_key),
-        DrimeUsagePercentageSensor(hass, api_key),
+        DrimeUsedSensor(usage_coordinator),
+        DrimeAvailableSensor(usage_coordinator),
+        DrimeTotalSensor(usage_coordinator),
+        DrimeUsagePercentageSensor(usage_coordinator),
         DrimeNotesSensor(notes_coordinator),
         DrimeNotesCountSensor(notes_coordinator),
         DrimeTrackedFilesSensor(tracked_files_coordinator)
